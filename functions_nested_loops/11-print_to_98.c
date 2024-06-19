739a1b91f95ca0b6402dd08fb0e2cfec92e09abb
Pirataninja777 <9483@holbertonstudents.com>
2024-06-19 17:49:11 -0400

    11-print_to_98.c

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -7,14 +7,20 @@
  */
 void print_to_98(int n)
 {
-    if (n <= 98) {
-        for (; n < 98; n++) {
-            printf("%d, ", n);
-        }
-    } else {
-        for (; n > 98; n--) {
-            printf("%d, ", n);
-        }
-    }
-    printf("98\n");
+	if (n >= 98)
+	{
+		while (n > 98)
+		{
+			printf("%d, ", n--);
+		}
+		printf("%d\n", n);
+	}
+	else
+	{
+		while (n < 98)
+		{
+			printf("%d, ", n++);
+		}
+		printf("%d\n", n);
+	}
 }
