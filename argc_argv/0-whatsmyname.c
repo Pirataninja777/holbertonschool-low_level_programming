c2abf618e32a3da096a4c87d936f4958a35d6a8b
Pirataninja777 <9483@holbertonstudents.com>
2024-07-02 12:29:48 -0400

    0-whatsmyname.c

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - imprime el nombre del programa
+ * @argc: número de argumentos de la línea de comandos (no se usa)
+ * @argv: arreglo de punteros a los argumentos de la línea de comandos
+ *
+ * Return: 0 al finalizar correctamente
+ */
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return 0;
+}
