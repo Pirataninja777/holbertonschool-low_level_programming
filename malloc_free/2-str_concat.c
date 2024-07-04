f4a36d8785dec9517fa2ec4c0bd92c6cd4ce518a
Pirataninja777 <9483@holbertonstudents.com>
2024-07-03 23:04:45 -0400

    2-str_concat

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -6,42 +6,35 @@
  * @s1: The first string.
  * @s2: The second string.
  *
- * Return: Pointer to a newly allocated space in memory containing the
- * concatenated string, or NULL on failure.
+ * Return: Pointer to a newly allocated
+ * space in memory containing the concatenated
+ * string, or NULL on failure.
  */
 char *str_concat(char *s1, char *s2)
 {
 	char *concat;
-	int len1 = s1 ? 0 : -1, len2 = s2 ? 0 : -1, i = 0, g = 0;
+	int len1 = 0, len2 = 0, i, j;
 
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
 
-	while (s1[len1 + 1])
+	while (s1[len1] != '\0')
 		len1++;
-	while (s2[len2 + 1])
+	while (s2[len2] != '\0')
 		len2++;
 
-	concat = malloc(sizeof(char) * (len1 + len2 + 2));
+	concat = malloc((len1 + len2 + 1) * sizeof(char));
 	if (concat == NULL)
 		return (NULL);
 
-	while (i <= len1)
-	{
+	for (i = 0; i < len1; i++)
 		concat[i] = s1[i];
-		i++;
-	}
+	for (j = 0; j < len2; j++)
+		concat[len1 + j] = s2[j];
 
-	while (i <= len1 + len2 + 1)
-	{
-		concat[i] = s2[g];
-		i++;
-		g++;
-	}
-
-	concat[i] = '\0';
+	concat[len1 + len2] = '\0';
 
 	return (concat);
 }
