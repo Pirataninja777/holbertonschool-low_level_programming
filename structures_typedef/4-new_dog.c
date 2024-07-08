6bea811aa4a0d2a45124df2e877b70f504a5d96a
Pirataninja777 <9483@holbertonstudents.com>
2024-07-08 17:44:07 -0400

    4-new_dog.c

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -40,3 +40,18 @@ dog_t *new_dog(char *name, float age, char *owner)
 
 	return (new_dog);
 }
+/**
+ * free_dog - frees memory allocated for a dog structure
+ * @d: pointer to dog structure
+ *
+ * Return: void
+ */
+void free_dog(dog_t *d)
+{
+	if (d != NULL)
+	{
+		free(d->name);
+		free(d->owner);
+		free(d);
+	}
+}
