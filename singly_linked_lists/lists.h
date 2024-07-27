b2cd27c6041342edc6d73d9d6b239065cf9c46d9
Pirataninja777 <9483@holbertonstudents.com>
2024-07-27 11:50:45 -0400

    betty style check

--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -1,3 +1,6 @@
+#ifndef LISTS_H
+#define LISTS_H
+
 #include <stdio.h>
 #include <stddef.h>
 /**
@@ -10,11 +13,12 @@
  */
 typedef struct list_s
 {
-    char *str;
-    unsigned int len;
-    struct list_s *next;
+char *str;
+unsigned int len;
+struct list_s *next;
 } list_t;
 
 /*function prototypes*/
 int _putchar(char c);
 size_t print_list(const list_t *h);
+#endif
